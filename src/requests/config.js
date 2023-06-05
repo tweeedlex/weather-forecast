6544f1759ac396af47d9e5dab20db3aee283479d
PozhilayaKrevetka192 <7545245@ukr.net>
2023-06-05 21:50:04 +0300

    fix

--- src/requests/config.js
+++ src/requests/config.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://api.weatherapi.com/v1";
+export const API_URL = "https://api.weatherapi.com/v1";
 export const CURRENT_URL = `${API_URL}/current.json`;
 export const FORECAST_URL = `${API_URL}/forecast.json`;
 export const HISTORY_URL = `${API_URL}/history.json`;
